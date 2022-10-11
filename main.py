16e85ae985cba4a95085817eb7db3c68708c5035
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 23:04:51 -0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
-
-# ******************************
-# Make your Code
-# ******************************
-
-# print (num3) 
+num1 = list(map(int, input("Enter the numbers ").split()))
+num2 = list(map(int, input("Enter the numbers ").split()))
+num3 = []
+i = 0
+if len(num1) <= len(num2):
+    for j in range(len(num1)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+        i+=1
+    for j in range (len(num2) - len(num1)):
+        num3.append(num2[i])
+        i+=1
+elif len(num1) > len(num2):
+    for j in range(len(num2)):
+        num3.append(num1[i])
+        num3.append(num2[i])
+        i+=1
+    for j in range (len(num1) - len(num2)):
+        num3.append(num1[i])
+        i+=1
+print(num3)
